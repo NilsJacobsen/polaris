125db400599eb187de0efda8cd6794dbd695dbd2
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-04-27 08:53:12 -0400

    Fix padding on AlphaFilter in IndexFilter when hideQueryField is true
    Closes #9101

--- polaris-react/src/components/AlphaFilters/AlphaFilters.tsx
+++ polaris-react/src/components/AlphaFilters/AlphaFilters.tsx
@@ -360,7 +360,7 @@ export function AlphaFilters({
           </div>
         </div>
         {hideQueryField ? (
-          <Box paddingInlineEnd="2" paddingBlockStart="2">
+          <Box paddingInlineEnd="2" paddingBlockStart="2" paddingBlockEnd="2">
             <HorizontalStack
               align="start"
               blockAlign="center"
